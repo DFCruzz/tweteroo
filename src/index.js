7222c3de9c01d513088779d86bc0e9df693d6598
DFCruzz <diegofcruzz@gmail.com>
2023-01-06 19:43:23 -0300

    setup server

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from "cors"
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+
+
+
+
+server.listen(5000, () => {
+    console.log("O servidor foi iniciado!")
+})
