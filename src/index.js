afa30e16a453f9d094bfc526b7707aa7bcbe347d
DFCruzz <diegofcruzz@gmail.com>
2023-01-09 13:54:33 -0300

    sign-up function added

--- src/index.js
+++ src/index.js
@@ -6,9 +6,31 @@ const server = express()
 server.use(express.json())
 server.use(cors())
 
+const PORT = 5000
+const loggedUser= []
+
+server.post("/sign-up", (req, res) => {
+    const { username, avatar } = req.body
+
+    const user = {
+        username: username,
+        avatar: avatar
+    }
+
+    if (!user.username || !user.avatar) {
+        return res.status(400).send("Todos os campos são obrigatórios!")
+    }
+
+    if (!(typeof user.username === "string") || !(typeof user.avatar === "string")) {
+        return res.Status(400).send("Campos inválidos!")
+    }
 
 
+    loggedUser.push(user)
+    res.sendStatus(200)
+})
 
-server.listen(5000, () => {
-    console.log("O servidor foi iniciado!")
+
+server.listen(PORT, () => {
+    console.log(`O servidor foi iniciado com sucesso! Porta: ${PORT}`)
 })
